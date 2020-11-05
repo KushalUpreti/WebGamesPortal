c6d7ce38ddf548f68f24379907be468607e7a450
Kushal Upreti <ehunt9030@gmail.com>
2020-11-05 22:57:48 +0545

    Deploying

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <div className="App">
         <Header />
 
-        <Route path="/WebGamesPortal" exact>
+        <Route path="/WebGamesPortal/" exact>
           <Homepage />
         </Route>
 
