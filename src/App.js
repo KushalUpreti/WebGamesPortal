47b048bdc16d05f5111800eddd124df9158b6421
Kushal Upreti <ehunt9030@gmail.com>
2020-11-05 23:14:42 +0545

    Link fix

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import CategoryPage from './pages/Containers/CategoryPage';
 function App() {
 
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/WebGamesPortal/">
 
       <div className="App">
         <Header />
